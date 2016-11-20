7c66ae77a25cffb7bf972957cf449d33e39ead13
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2016-11-20 11:08:32 +0100

    Add RDF loading/unloading scripts

---
+++ sql/unload.sql
@@ -0,0 +1,19 @@
+log_enable(2);
+
+sparql clear graph pubchem:compound;
+sparql clear graph pubchem:substance;
+sparql clear graph pubchem-descriptor:compound;
+sparql clear graph pubchem-descriptor:substance;
+sparql clear graph pubchem:synonym;
+sparql clear graph pubchem:inchikey;
+sparql clear graph pubchem:measuregroup;
+sparql clear graph pubchem:endpoint;
+sparql clear graph pubchem:bioassay;
+sparql clear graph pubchem:protein;
+sparql clear graph pubchem:biosystem;
+sparql clear graph pubchem:conserveddomain;
+sparql clear graph pubchem:gene;
+sparql clear graph pubchem:reference;
+sparql clear graph pubchem:source;
+sparql clear graph pubchem:concept;
+sparql clear graph pubchem:void;
