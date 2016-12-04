2b7c70b3db5c81550044cbf2d5247fe08a573d7f
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2016-12-04 09:20:36 +0100

    Change the primary key for bioassay_data

--- sql/tables/bioassay.sql
+++ sql/tables/bioassay.sql
@@ -18,14 +18,12 @@ grant select on bioassay_bases to "SPARQL";
 
 create table bioassay_data
 (
-    __          integer identity,
     bioassay    integer not null,
     type        smallint not null,
     value       long nvarchar not null,
-    primary key(__)
+    primary key(bioassay, type)
 );
 
-create unique index bioassay_data__bioassay_type on bioassay_data(bioassay, type);
 create index bioassay_data__bioassay on bioassay_data(bioassay);
 create bitmap index bioassay_data__type on bioassay_data(type);
 grant select on bioassay_data to "SPARQL";
