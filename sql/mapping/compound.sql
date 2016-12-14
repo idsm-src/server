e626c57d2d26583749f8b10b9cfc2a024c0dc600
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2016-12-14 11:59:03 +0100

    Add compound descriptor translation scripts

--- sql/mapping/compound.sql
+++ sql/mapping/compound.sql
@@ -1,11 +1,19 @@
 sparql
 alter quad storage virtrdf:PubchemQuadStorage
-    from DB.rdf.compound_bases              as compound_bases
-    from DB.rdf.compound_relations          as compound_relations
-    from DB.rdf.compound_roles              as compound_roles
-    from DB.rdf.compound_biosystems         as compound_biosystems
-    from DB.rdf.compound_types              as compound_types
-    from DB.rdf.compound_active_ingredients as compound_active_ingredients
+    from DB.rdf.compound_bases                                 as compound_bases
+    from DB.rdf.compound_relations                             as compound_relations
+    from DB.rdf.compound_roles                                 as compound_roles
+    from DB.rdf.compound_biosystems                            as compound_biosystems
+    from DB.rdf.compound_types                                 as compound_types
+    from DB.rdf.compound_active_ingredients                    as compound_active_ingredients
+    from DB.rdf.descriptor_compound_bases                      as descriptor_compound_bases
+    from DB.rdf.descriptor_compound_molecular_formulas         as descriptor_compound_molecular_formulas
+    from DB.rdf.descriptor_compound_isomeric_smileses          as descriptor_compound_isomeric_smileses
+    from DB.rdf.descriptor_compound_canonical_smileses         as descriptor_compound_canonical_smileses
+    from DB.rdf.descriptor_compound_iupac_inchis               as descriptor_compound_iupac_inchis
+    from DB.rdf.descriptor_compound_iupac_inchis_long          as descriptor_compound_iupac_inchis_long
+    from DB.rdf.descriptor_compound_preferred_iupac_names      as descriptor_compound_preferred_iupac_names
+    from DB.rdf.descriptor_compound_preferred_iupac_names_long as descriptor_compound_preferred_iupac_names_long
 {
     create map:compound as graph pubchem:compound option (exclusive)
     {
@@ -31,5 +39,81 @@ alter quad storage virtrdf:PubchemQuadStorage
 
         iri:compound(compound_active_ingredients.compound)
             vocab:is_active_ingredient_of iri:compound_type(compound_active_ingredients.unit, compound_active_ingredients.ingredient) .
+
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_hydrogen_bond_acceptor_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.hydrogen_bond_acceptor_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_tautomer_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.tautomer_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_defined_atom_stereo_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.defined_atom_stereo_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_defined_bond_stereo_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.defined_bond_stereo_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_undefined_bond_stereo_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.undefined_bond_stereo_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_isotope_atom_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.isotope_atom_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_covalent_unit_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.covalent_unit_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_hydrogen_bond_donor_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.hydrogen_bond_donor_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_non_hydrogen_atom_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.non_hydrogen_atom_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_rotatable_bond_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.rotatable_bond_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_undefined_atom_stereo_count(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.undefined_atom_stereo_count is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_total_formal_charge(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.total_formal_charge is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_structure_complexity(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.structure_complexity is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_mono_isotopic_weight(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.mono_isotopic_weight is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_xlogp3_aa(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.xlogp3_aa is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_exact_mass(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.exact_mass is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_molecular_weight(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.molecular_weight is not null) .
+
+        iri:compound(descriptor_compound_bases.compound)
+            sio:has-attribute iri:descriptor_tpsa(descriptor_compound_bases.compound) where (^{descriptor_compound_bases.}^.tpsa is not null) .
+
+        iri:compound(descriptor_compound_molecular_formulas.compound)
+            sio:has-attribute iri:descriptor_molecular_formula(descriptor_compound_molecular_formulas.compound) .
+
+        iri:compound(descriptor_compound_isomeric_smileses.compound)
+            sio:has-attribute iri:descriptor_isomeric_smiles(descriptor_compound_isomeric_smileses.compound) .
+
+        iri:compound(descriptor_compound_canonical_smileses.compound)
+            sio:has-attribute iri:descriptor_canonical_smiles(descriptor_compound_canonical_smileses.compound) .
+
+        iri:compound(descriptor_compound_iupac_inchis.compound)
+            sio:has-attribute iri:descriptor_iupac_inchi(descriptor_compound_iupac_inchis.compound) .
+
+        iri:compound(descriptor_compound_iupac_inchis_long.compound)
+            sio:has-attribute iri:descriptor_iupac_inchi(descriptor_compound_iupac_inchis_long.compound) .
+
+        iri:compound(descriptor_compound_preferred_iupac_names.compound)
+            sio:has-attribute iri:descriptor_preferred_iupac_name(descriptor_compound_preferred_iupac_names.compound) .
+
+        iri:compound(descriptor_compound_preferred_iupac_names_long.compound)
+            sio:has-attribute iri:descriptor_preferred_iupac_name(descriptor_compound_preferred_iupac_names_long.compound) .
     }.
 }.;
---
+++ sql/mapping/descriptor_compound.sql
@@ -0,0 +1,118 @@
+sparql
+alter quad storage virtrdf:PubchemQuadStorage
+    from DB.rdf.descriptor_compound_bases                      as descriptor_compound_bases
+    from DB.rdf.descriptor_compound_molecular_formulas         as descriptor_compound_molecular_formulas
+    from DB.rdf.descriptor_compound_isomeric_smileses          as descriptor_compound_isomeric_smileses
+    from DB.rdf.descriptor_compound_canonical_smileses         as descriptor_compound_canonical_smileses
+    from DB.rdf.descriptor_compound_iupac_inchis               as descriptor_compound_iupac_inchis
+    from DB.rdf.descriptor_compound_iupac_inchis_long          as descriptor_compound_iupac_inchis_long
+    from DB.rdf.descriptor_compound_preferred_iupac_names      as descriptor_compound_preferred_iupac_names
+    from DB.rdf.descriptor_compound_preferred_iupac_names_long as descriptor_compound_preferred_iupac_names_long
+{
+    create map:descriptor_compound as graph descriptor:compound option (exclusive)
+    {
+        iri:descriptor_hydrogen_bond_acceptor_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000388 where (^{descriptor_compound_bases.}^.hydrogen_bond_acceptor_count is not null) ;
+            sio:has-value descriptor_compound_bases.hydrogen_bond_acceptor_count .
+
+        iri:descriptor_tautomer_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000391 where (^{descriptor_compound_bases.}^.tautomer_count is not null) ;
+            sio:has-value descriptor_compound_bases.tautomer_count .
+
+        iri:descriptor_defined_atom_stereo_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000370 where (^{descriptor_compound_bases.}^.defined_atom_stereo_count is not null) ;
+            sio:has-value descriptor_compound_bases.defined_atom_stereo_count .
+
+        iri:descriptor_defined_bond_stereo_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000371 where (^{descriptor_compound_bases.}^.defined_bond_stereo_count is not null) ;
+            sio:has-value descriptor_compound_bases.defined_bond_stereo_count .
+
+        iri:descriptor_undefined_bond_stereo_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000375 where (^{descriptor_compound_bases.}^.undefined_bond_stereo_count is not null) ;
+            sio:has-value descriptor_compound_bases.undefined_bond_stereo_count .
+
+        iri:descriptor_isotope_atom_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000372 where (^{descriptor_compound_bases.}^.isotope_atom_count is not null) ;
+            sio:has-value descriptor_compound_bases.isotope_atom_count .
+
+        iri:descriptor_covalent_unit_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000369 where (^{descriptor_compound_bases.}^.covalent_unit_count is not null) ;
+            sio:has-value descriptor_compound_bases.covalent_unit_count .
+
+        iri:descriptor_hydrogen_bond_donor_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000387 where (^{descriptor_compound_bases.}^.hydrogen_bond_donor_count is not null) ;
+            sio:has-value descriptor_compound_bases.hydrogen_bond_donor_count .
+
+        iri:descriptor_non_hydrogen_atom_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000373 where (^{descriptor_compound_bases.}^.non_hydrogen_atom_count is not null) ;
+            sio:has-value descriptor_compound_bases.non_hydrogen_atom_count .
+
+        iri:descriptor_rotatable_bond_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000389 where (^{descriptor_compound_bases.}^.rotatable_bond_count is not null) ;
+            sio:has-value descriptor_compound_bases.rotatable_bond_count .
+
+        iri:descriptor_undefined_atom_stereo_count(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000374 where (^{descriptor_compound_bases.}^.undefined_atom_stereo_count is not null) ;
+            sio:has-value descriptor_compound_bases.undefined_atom_stereo_count .
+
+        iri:descriptor_total_formal_charge(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000336 where (^{descriptor_compound_bases.}^.total_formal_charge is not null) ;
+            sio:has-value descriptor_compound_bases.total_formal_charge .
+
+        iri:descriptor_structure_complexity(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000390 where (^{descriptor_compound_bases.}^.structure_complexity is not null) ;
+            sio:has-value descriptor_compound_bases.structure_complexity .
+
+        iri:descriptor_mono_isotopic_weight(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000337 where (^{descriptor_compound_bases.}^.mono_isotopic_weight is not null) ;
+            sio:has-unit obo:UO_0000055 where (^{descriptor_compound_bases.}^.mono_isotopic_weight is not null) ;
+            sio:has-value descriptor_compound_bases.mono_isotopic_weight .
+
+        iri:descriptor_xlogp3_aa(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000395 where (^{descriptor_compound_bases.}^.xlogp3_aa is not null) ;
+            sio:has-value descriptor_compound_bases.xlogp3_aa .
+
+        iri:descriptor_exact_mass(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000338 where (^{descriptor_compound_bases.}^.exact_mass is not null) ;
+            sio:has-unit obo:UO_0000055 where (^{descriptor_compound_bases.}^.exact_mass is not null) ;
+            sio:has-value descriptor_compound_bases.exact_mass .
+
+        iri:descriptor_molecular_weight(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000334 where (^{descriptor_compound_bases.}^.molecular_weight is not null) ;
+            sio:has-unit obo:UO_0000055 where (^{descriptor_compound_bases.}^.molecular_weight is not null) ;
+            sio:has-value descriptor_compound_bases.molecular_weight .
+
+        iri:descriptor_tpsa(descriptor_compound_bases.compound)
+            rdf:type sio:CHEMINF_000392 where (^{descriptor_compound_bases.}^.tpsa is not null) ;
+            sio:has-unit obo:UO_0000324 where (^{descriptor_compound_bases.}^.tpsa is not null) ;
+            sio:has-value descriptor_compound_bases.tpsa .
+
+        iri:descriptor_molecular_formula(descriptor_compound_molecular_formulas.compound)
+            rdf:type sio:CHEMINF_000335 ;
+            sio:has-value descriptor_compound_molecular_formulas.molecular_formula .
+
+        iri:descriptor_isomeric_smiles(descriptor_compound_isomeric_smileses.compound)
+            rdf:type sio:CHEMINF_000379 ;
+            sio:has-value descriptor_compound_isomeric_smileses.isomeric_smiles .
+
+        iri:descriptor_canonical_smiles(descriptor_compound_canonical_smileses.compound)
+            rdf:type sio:CHEMINF_000376 ;
+            sio:has-value descriptor_compound_canonical_smileses.canonical_smiles .
+
+        iri:descriptor_iupac_inchi(descriptor_compound_iupac_inchis.compound)
+            rdf:type sio:CHEMINF_000396 ;
+            sio:has-value descriptor_compound_iupac_inchis.iupac_inchi .
+
+        iri:descriptor_iupac_inchi(descriptor_compound_iupac_inchis_long.compound)
+            rdf:type sio:CHEMINF_000396 ;
+            sio:has-value descriptor_compound_iupac_inchis_long.iupac_inchi .
+
+        iri:descriptor_preferred_iupac_name(descriptor_compound_preferred_iupac_names.compound)
+            rdf:type sio:CHEMINF_000382 ;
+            sio:has-value descriptor_compound_preferred_iupac_names.preferred_iupac_name .
+
+        iri:descriptor_preferred_iupac_name(descriptor_compound_preferred_iupac_names_long.compound)
+            rdf:type sio:CHEMINF_000382 ;
+            sio:has-value descriptor_compound_preferred_iupac_names_long.preferred_iupac_name .
+    }.
+}.;
