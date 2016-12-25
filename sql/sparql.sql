6c4208cb0f20d5913f1a462b9a416531ce907cb1
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2016-12-25 09:08:13 +0100

    Add a script to set the SPARQL account

---
+++ sql/sparql.sql
@@ -0,0 +1,6 @@
+update DB.DBA.SYS_USERS set U_PASSWORD='neaztaktajneheslo' where U_NAME='SPARQL';
+USER_SET_OPTION ('SPARQL', 'DISABLED', 0);
+db..user_set_qualifier ('SPARQL', 'DB');
+
+
+grant execute on DB.DBA.RL_I2ID TO "SPARQL";
