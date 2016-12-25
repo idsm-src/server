3496bc0b5baf367bfe527cc4c1ac37e56cf0e4a2
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2016-12-25 08:43:19 +0100

    Add the integer primary key to bioassay_data
    The primary key of bioassay_data has to be an integer due to full-text
    indexing.

--- sql/tables/bioassay.sql
+++ sql/tables/bioassay.sql
@@ -18,12 +18,15 @@ grant select on bioassay_bases to "SPARQL";
 
 create table bioassay_data
 (
+    __          integer identity,
     bioassay    integer not null,
     type        smallint not null,
     value       long nvarchar not null,
-    primary key(bioassay, type)
+    primary key(__)
 );
 
+
+create index bioassay_data__bioassay_type on bioassay_data(bioassay, type);
 create index bioassay_data__bioassay on bioassay_data(bioassay);
 create bitmap index bioassay_data__type on bioassay_data(type);
 grant select on bioassay_data to "SPARQL";
