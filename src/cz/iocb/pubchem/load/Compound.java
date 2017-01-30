27848176d3f16ccf77ad83a789a0c47f0a59bc7d
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-01-31 00:09:42 +0100

    Modify the java compound loader to use the stream table loader

--- src/cz/iocb/pubchem/load/Compound.java
+++ src/cz/iocb/pubchem/load/Compound.java
@@ -1,11 +1,12 @@
 package cz.iocb.pubchem.load;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import org.apache.jena.graph.Node;
 import cz.iocb.pubchem.load.common.Loader;
-import cz.iocb.pubchem.load.common.PubchemFileTableLoader;
+import cz.iocb.pubchem.load.common.StreamTableLoader;
 
 
 
@@ -13,75 +14,71 @@ public class Compound extends Loader
 {
     private static void loadBiosystems(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader, "insert into compound_biosystems(compound, biosystem) values (?,?)")
+        new StreamTableLoader(stream, "insert into compound_biosystems(compound, biosystem) values (?,?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("obo:BFO_0000056"))
+                if(!predicate.getURI().equals("http://purl.obolibrary.org/obo/BFO_0000056"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "biosystem:BSID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/biosystem/BSID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadComponents(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_relations(compound_from, relation, compound_to) values (?, 480, ?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("sio:CHEMINF_000480"))
+                if(!predicate.getURI().equals("http://semanticscience.org/resource/CHEMINF_000480"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadDrugproducts(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_active_ingredients(compound, unit, ingredient) values (?, ?, ?)")
         {
-            {
-                prefixes.put("ns2:", "<http://purl.bioontology.org/ontology/NDFRT/>");
-            }
-
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("vocab:is_active_ingredient_of"))
+                if(!predicate.getURI().equals("http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#is_active_ingredient_of"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
 
-                if(object.startsWith("<http://purl.bioontology.org/ontology/SNOMEDCT/"))
+                if(object.getURI().startsWith("http://purl.bioontology.org/ontology/SNOMEDCT/"))
                 {
                     setValue(2, 1);
-                    setValue(3, getIntID(object, "<http://purl.bioontology.org/ontology/SNOMEDCT/", ">"));
+                    setValue(3, getIntID(object, "http://purl.bioontology.org/ontology/SNOMEDCT/"));
                 }
-                else if(object.startsWith("ns2:N"))
+                else if(object.getURI().startsWith("http://purl.bioontology.org/ontology/NDFRT/N"))
                 {
                     setValue(2, 2);
-                    setValue(3, getIntID(object, "ns2:N"));
+                    setValue(3, getIntID(object, "http://purl.bioontology.org/ontology/NDFRT/N"));
                 }
                 else
                 {
@@ -90,158 +87,154 @@ public class Compound extends Loader
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadIsotopologues(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_relations(compound_from, relation, compound_to) values (?, 455, ?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("sio:CHEMINF_000455"))
+                if(!predicate.getURI().equals("http://semanticscience.org/resource/CHEMINF_000455"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadParents(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_relations(compound_from, relation, compound_to) values (?, 1024, ?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("vocab:has_parent"))
+                if(!predicate.getURI().equals("http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#has_parent"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadSameConnectivities(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_relations(compound_from, relation, compound_to) values (?, 462, ?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("sio:CHEMINF_000462"))
+                if(!predicate.getURI().equals("http://semanticscience.org/resource/CHEMINF_000462"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadStereoisomers(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader,
+        new StreamTableLoader(stream,
                 "insert into compound_relations(compound_from, relation, compound_to) values (?, 461, ?)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("sio:CHEMINF_000461"))
+                if(!predicate.getURI().equals("http://semanticscience.org/resource/CHEMINF_000461"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
-                setValue(2, getIntID(object, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
+                setValue(2, getIntID(object, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadRoles(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader, "insert into compound_roles(compound, roleid) values (?, 0)")
+        new StreamTableLoader(stream, "insert into compound_roles(compound, roleid) values (?, 0)")
         {
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("obo:has-role") || !object.equals("vocab:FDAApprovedDrugs"))
+                if(!predicate.getURI().equals("http://purl.obolibrary.org/obo/has-role")
+                        || !object.getURI().equals("http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#FDAApprovedDrugs"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
     private static void loadTypes(String file) throws IOException, SQLException
     {
-        BufferedReader reader = getReader(file);
+        InputStream stream = getStream(file);
 
-        new PubchemFileTableLoader(reader, "insert into compound_types(compound, unit, type) values (?, ?, ?)")
+        new StreamTableLoader(stream, "insert into compound_types(compound, unit, type) values (?, ?, ?)")
         {
-            {
-                prefixes.put("ns4:", "<http://purl.bioontology.org/ontology/NDFRT/>");
-                prefixes.put("nci:", "<http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#>");
-            }
-
             @Override
-            public void insert(String subject, String predicate, String object) throws SQLException, IOException
+            public void insert(Node subject, Node predicate, Node object) throws SQLException, IOException
             {
-                if(!predicate.equals("rdf:type"))
+                if(!predicate.getURI().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))
                     throw new IOException();
 
-                setValue(1, getIntID(subject, "compound:CID"));
+                setValue(1, getIntID(subject, "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID"));
 
-                if(object.startsWith("obo:CHEBI_"))
+                if(object.getURI().startsWith("http://purl.obolibrary.org/obo/CHEBI_"))
                 {
                     setValue(2, 0);
-                    setValue(3, getIntID(object, "obo:CHEBI_"));
+                    setValue(3, getIntID(object, "http://purl.obolibrary.org/obo/CHEBI_"));
                 }
-                else if(object.startsWith("<http://purl.bioontology.org/ontology/SNOMEDCT/"))
+                else if(object.getURI().startsWith("http://purl.bioontology.org/ontology/SNOMEDCT/"))
                 {
                     setValue(2, 1);
-                    setValue(3, getIntID(object, "<http://purl.bioontology.org/ontology/SNOMEDCT/", ">"));
+                    setValue(3, getIntID(object, "http://purl.bioontology.org/ontology/SNOMEDCT/"));
                 }
-                else if(object.startsWith("ns4:N"))
+                else if(object.getURI().startsWith("http://purl.bioontology.org/ontology/NDFRT/N"))
                 {
                     setValue(2, 2);
-                    setValue(3, getIntID(object, "ns4:N"));
+                    setValue(3, getIntID(object, "http://purl.bioontology.org/ontology/NDFRT/N"));
                 }
-                else if(object.startsWith("nci:C"))
+                else if(object.getURI().startsWith("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C"))
                 {
                     setValue(2, 3);
-                    setValue(3, getIntID(object, "nci:C"));
+                    setValue(3, getIntID(object, "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C"));
                 }
-                else if(object.equals("bp:SmallMolecule"))
+                else if(object.getURI().equals("http://www.biopax.org/release/biopax-level3.owl#SmallMolecule"))
                 {
                     setValue(2, 4);
                     setValue(3, -1);
@@ -253,7 +246,7 @@ public class Compound extends Loader
             }
         }.load();
 
-        reader.close();
+        stream.close();
     }
 
 
