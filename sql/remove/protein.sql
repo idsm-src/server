b64fac3df00c195a17194e8c90a1d22649a79abe
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-02-02 13:04:21 +0100

    Add missing semicolons

--- sql/remove/protein.sql
+++ sql/remove/protein.sql
@@ -14,17 +14,17 @@ drop function iri_protein_INVERSE;
 
 --------------------------------------------------------------------------------
 
-drop table protein_complexes
-drop table protein_types
-drop table protein_locations
-drop table protein_functions
-drop table protein_participates_biosystems
-drop table protein_participates_goes
-drop table protein_continuantparts
-drop table protein_conserveddomains
-drop table protein_closematches
-drop table protein_genes
-drop table protein_similarproteins
-drop table protein_pdblinks
-drop table protein_references
-drop table protein_bases
+drop table protein_complexes;
+drop table protein_types;
+drop table protein_locations;
+drop table protein_functions;
+drop table protein_participates_biosystems;
+drop table protein_participates_goes;
+drop table protein_continuantparts;
+drop table protein_conserveddomains;
+drop table protein_closematches;
+drop table protein_genes;
+drop table protein_similarproteins;
+drop table protein_pdblinks;
+drop table protein_references;
+drop table protein_bases;
