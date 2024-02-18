f58895d56f0d39ecf2652ad0140ad7cd2fdb0610
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2024-02-18 10:00:45 +0100

    Unify format of sql function code

--- sql/chembl/functions/molecule_reference.sql
+++ sql/chembl/functions/molecule_reference.sql
@@ -1,18 +1,19 @@
-create function chembl_tmp.url_decode(input text) returns text language plpgsql as $$
-declare
-  bin bytea = '';
-  byte text;
-begin
-  for byte in (select (regexp_matches(input, '(%..|.)', 'g'))[1]) loop
-    if length(byte) = 3 then
-      bin = bin || decode(substring(byte, 2, 2), 'hex');
-    elseif  byte = '+' then
-      bin = bin || ' '::bytea;
-    else
-      bin = bin || byte::bytea;
-    end if;
-  end loop;
-  return convert_from(bin, 'utf8');
-end
+create function chembl_tmp.url_decode(input text) returns text language plpgsql as
+$$
+  declare
+    bin bytea = '';
+    byte text;
+  begin
+    for byte in (select (regexp_matches(input, '(%..|.)', 'g'))[1]) loop
+      if length(byte) = 3 then
+        bin = bin || decode(substring(byte, 2, 2), 'hex');
+      elseif  byte = '+' then
+        bin = bin || ' '::bytea;
+      else
+        bin = bin || byte::bytea;
+      end if;
+    end loop;
+    return convert_from(bin, 'utf8');
+  end
 $$
 immutable strict parallel safe;
