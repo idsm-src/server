8134b624e8248228a97e9fc7eac8eda595c39a32
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2025-07-23 09:59:17 +0200

    Optimize void mapping by adding extra conditions

--- config/src/main/java/cz/iocb/chemweb/server/sparql/config/stats/Void.java
+++ config/src/main/java/cz/iocb/chemweb/server/sparql/config/stats/Void.java
@@ -6,6 +6,7 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import cz.iocb.chemweb.server.sparql.config.ontology.Ontology;
 import cz.iocb.sparql.engine.database.Conditions;
 import cz.iocb.sparql.engine.database.Table;
 import cz.iocb.sparql.engine.database.TableColumn;
@@ -206,46 +207,47 @@ public class Void
 
         {
             Table table = new Table(schema, "class_partitions");
+            Conditions cnd = config.createAreNotEqualCondition("class_unit", Ontology.unitCHEBI, Ontology.unitTaxonomy);
 
             NodeMapping dataset = config.createIriMapping("void:class-partition", "graph", "class_unit", "class_id");
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("rdf:type"),
-                    config.createIriMapping("void:Dataset"));
+                    config.createIriMapping("void:Dataset"), cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:graph", "graph"),
-                    config.createIriMapping("void:subset"), dataset);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:graph", "graph"),
-                    config.createIriMapping("void:classPartition"), dataset);
+                    config.createIriMapping("void:classPartition"), dataset, cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:class"),
-                    config.createIriMapping("ontology:resource", "class_unit", "class_id"));
+                    config.createIriMapping("ontology:resource", "class_unit", "class_id"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:triples"),
-                    config.createLiteralMapping(xsdLong, "triples"));
+                    config.createLiteralMapping(xsdLong, "triples"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:classes"),
-                    config.createLiteralMapping(xsdLong, "classes"));
+                    config.createLiteralMapping(xsdLong, "classes"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:properties"),
-                    config.createLiteralMapping(xsdLong, "properties"));
+                    config.createLiteralMapping(xsdLong, "properties"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctObjects"),
-                    config.createLiteralMapping(xsdLong, "objects"));
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceObjects"),
-                    config.createLiteralMapping(xsdLong, "iri_objects"));
+                    config.createLiteralMapping(xsdLong, "iri_objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void_ext:distinctLiterals"),
-                    config.createLiteralMapping(xsdLong, "literal_objects"));
+                    config.createLiteralMapping(xsdLong, "literal_objects"), cnd);
         }
 
         {
@@ -289,177 +291,183 @@ public class Void
 
         {
             Table table = new Table(schema, "class_property_partitions");
+            Conditions cnd = config.createAreNotEqualCondition("class_unit", Ontology.unitCHEBI, Ontology.unitTaxonomy);
 
             NodeMapping dataset = config.createIriMapping("void:class-property-partition", "graph", "class_unit",
                     "class_id", "property_unit", "property_id");
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("rdf:type"),
-                    config.createIriMapping("void:Dataset"));
+                    config.createIriMapping("void:Dataset"), cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:graph", "graph"),
-                    config.createIriMapping("void:subset"), dataset);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:property-partition", "graph", "property_unit", "property_id"),
-                    config.createIriMapping("void:subset"), dataset);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-partition", "graph", "class_unit", "class_id"),
-                    config.createIriMapping("void:subset"), dataset);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-partition", "graph", "class_unit", "class_id"),
-                    config.createIriMapping("void:propertyPartition"), dataset);
+                    config.createIriMapping("void:propertyPartition"), dataset, cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:property"),
-                    config.createIriMapping("ontology:resource", "property_unit", "property_id"));
+                    config.createIriMapping("ontology:resource", "property_unit", "property_id"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:triples"),
-                    config.createLiteralMapping(xsdLong, "triples"));
+                    config.createLiteralMapping(xsdLong, "triples"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctObjects"),
-                    config.createLiteralMapping(xsdLong, "objects"));
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceObjects"),
-                    config.createLiteralMapping(xsdLong, "iri_objects"));
+                    config.createLiteralMapping(xsdLong, "iri_objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void_ext:distinctLiterals"),
-                    config.createLiteralMapping(xsdLong, "literal_objects"));
+                    config.createLiteralMapping(xsdLong, "literal_objects"), cnd);
         }
 
         {
             Table table = new Table(schema, "linksets");
+            Conditions cnds = config.createAreNotEqualCondition("subject_unit", Ontology.unitCHEBI,
+                    Ontology.unitTaxonomy);
+            Conditions cndo = config.createAreNotEqualCondition("object_unit", Ontology.unitCHEBI,
+                    Ontology.unitTaxonomy);
+            Conditions cnd = Conditions.and(cnds, cndo);
 
             NodeMapping dataset = config.createIriMapping("void:linkset", "property_graph", "property_unit",
                     "property_id", "subject_graph", "subject_unit", "subject_id", "object_graph", "object_unit",
                     "object_id");
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("rdf:type"),
-                    config.createIriMapping("void:Dataset"));
+                    config.createIriMapping("void:Dataset"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("rdf:type"),
-                    config.createIriMapping("void:Linkset"));
+                    config.createIriMapping("void:Linkset"), cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:graph", "property_graph"),
-                    config.createIriMapping("void:subset"), dataset);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:property-partition", "property_graph",
-                    "property_unit", "property_id"), config.createIriMapping("void:subset"), dataset);
+                    "property_unit", "property_id"), config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"),
                     config.createIriMapping("void:subset"), dataset,
-                    config.createAreEqualCondition("subject_graph", "property_graph"));
+                    Conditions.and(cnd, config.createAreEqualCondition("subject_graph", "property_graph")));
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-property-partition", "property_graph", "subject_unit",
                             "subject_id", "property_unit", "property_id"),
                     config.createIriMapping("void:subset"), dataset,
-                    config.createAreEqualCondition("subject_graph", "property_graph"));
-
-            config.addQuadMapping(table, graph, config.createIriMapping("void:property-partition", "property_graph",
-                    "property_unit", "property_id"), config.createIriMapping("void:subset"), dataset);
+                    Conditions.and(cnd, config.createAreEqualCondition("subject_graph", "property_graph")));
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:target"),
-                    config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"));
+                    config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"),
+                    cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:subjectsTarget"),
-                    config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"));
+                    config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"),
+                    cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:target"),
                     config.createIriMapping("void:class-partition", "object_graph", "object_unit", "object_id"),
                     Conditions.and(config.createAreNotEqualCondition("subject_graph", "object_graph"),
                             config.createAreNotEqualCondition("subject_unit", "object_unit"),
-                            config.createAreNotEqualCondition("subject_id", "object_id")));
+                            config.createAreNotEqualCondition("subject_id", "object_id"), cnd));
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:objectsTarget"),
-                    config.createIriMapping("void:class-partition", "object_graph", "object_unit", "object_id"));
+                    config.createIriMapping("void:class-partition", "object_graph", "object_unit", "object_id"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:linkPredicate"),
-                    config.createIriMapping("ontology:resource", "property_unit", "property_id"));
+                    config.createIriMapping("ontology:resource", "property_unit", "property_id"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:triples"),
-                    config.createLiteralMapping(xsdLong, "triples"));
+                    config.createLiteralMapping(xsdLong, "triples"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctObjects"),
-                    config.createLiteralMapping(xsdLong, "objects"));
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"));
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceObjects"),
-                    config.createLiteralMapping(xsdLong, "objects"));
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void_ext:distinctLiterals"),
-                    config.createLiteralMapping(0l));
+                    config.createLiteralMapping(0l), cnd);
         }
 
         {
             Table table = new Table(schema, "literal_linksets");
-
-            Conditions condition = config.createAreEqualCondition("subject_graph", "property_graph");
+            Conditions cnd = Conditions.and(
+                    config.createAreNotEqualCondition("subject_unit", Ontology.unitCHEBI, Ontology.unitTaxonomy),
+                    config.createAreEqualCondition("subject_graph", "property_graph"));
 
             NodeMapping dataset = config.createIriMapping("void:class-property-datatype-partition", "subject_graph",
                     "subject_unit", "subject_id", "property_unit", "property_id", "datatype_unit", "datatype_id");
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("rdf:type"),
-                    config.createIriMapping("void:Dataset"), condition);
+                    config.createIriMapping("void:Dataset"), cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:graph", "subject_graph"),
-                    config.createIriMapping("void:subset"), dataset, condition);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph, config.createIriMapping("void:property-partition", "property_graph",
-                    "property_unit", "property_id"), config.createIriMapping("void:subset"), dataset, condition);
+                    "property_unit", "property_id"), config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-partition", "subject_graph", "subject_unit", "subject_id"),
-                    config.createIriMapping("void:subset"), dataset, condition);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-property-partition", "subject_graph", "subject_unit",
                             "subject_id", "property_unit", "property_id"),
-                    config.createIriMapping("void:subset"), dataset, condition);
+                    config.createIriMapping("void:subset"), dataset, cnd);
 
             config.addQuadMapping(table, graph,
                     config.createIriMapping("void:class-property-partition", "subject_graph", "subject_unit",
                             "subject_id", "property_unit", "property_id"),
-                    config.createIriMapping("void_ext:datatypePartition"), dataset, condition);
+                    config.createIriMapping("void_ext:datatypePartition"), dataset, cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void_ext:datatype"),
-                    config.createIriMapping("ontology:resource", "datatype_unit", "datatype_id"), condition);
+                    config.createIriMapping("ontology:resource", "datatype_unit", "datatype_id"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:triples"),
-                    config.createLiteralMapping(xsdLong, "triples"), condition);
+                    config.createLiteralMapping(xsdLong, "triples"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"), condition);
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void:distinctObjects"),
-                    config.createLiteralMapping(xsdLong, "objects"), condition);
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceSubjects"),
-                    config.createLiteralMapping(xsdLong, "subjects"), condition);
+                    config.createLiteralMapping(xsdLong, "subjects"), cnd);
 
             config.addQuadMapping(table, graph, dataset,
                     config.createIriMapping("void_ext:distinctIRIReferenceObjects"), config.createLiteralMapping(0l),
-                    condition);
+                    cnd);
 
             config.addQuadMapping(table, graph, dataset, config.createIriMapping("void_ext:distinctLiterals"),
-                    config.createLiteralMapping(xsdLong, "objects"), condition);
+                    config.createLiteralMapping(xsdLong, "objects"), cnd);
         }
     }
 }
